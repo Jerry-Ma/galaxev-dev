5d196673e6a706b1514311ce4015fa7fa1056bb9
Jerry-Ma <jerry.ma.nk@gmail.com>
2013-03-22 17:08:09 -0500

    a little tidier

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import bc03
 
 def csp(par):
     ui = draw.UI(par=par)  # instantize a main window
-    ui.master.title("Lib_View")
+    ui.master.title("Composation of Stellar Populations")
     ui.pack()
     ui.mainloop()
+
 
 def gpl(par):
     ui = draw.UI(par=par)
-    ui.master.title("Parspace View")
+    ui.master.title("View of Spectrum")
     ui.pack()
     ui.mainloop()
 
-def flt(par)
+
+def flt(par):
+    pass
+
 
 par = bc03.Cfg()
 csp(par)
